05d34062d5d0284e6acf349bb81abb7fd31f6791
Ali Pokrywka <alipokrywka@gmail.com>
2018-02-19 06:05:57 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*global $*/
+
+$(document).ready(function(){
+ $('.header').height($(window).height());
+ 
+ $(".navbar a").click(function(){
+  $("body,html").animate({
+   scrollTop:$("#" + $(this).data('value')).offset().top
+  },1000)
+  
+ })
+ 
+})
